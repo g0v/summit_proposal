34541e3d4271d297e145f38377ca879b05702ab7
UnaLai <qqq323618@gmail.com>
2020-05-25 23:35:52 +0800

    [feat]:install bootstrap and set it

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from "./store";
 
 Vue.config.productionTip = false;
 
+// 引入 Bootstrap 套件
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import { NavbarPlugin } from "bootstrap-vue";
+Vue.use(NavbarPlugin);
+
 new Vue({
   router,
   store,
