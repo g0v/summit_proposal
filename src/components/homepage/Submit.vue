6c067640b787bbcf3541605d7adf1e24efb64958
UnaLai <qqq323618@gmail.com>
2020-05-26 11:27:37 +0800

    [feat]:add Submit contant(less)

---
+++ src/components/homepage/Submit.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="submit">
+    <div class="submit-container container">
+      <h2 class="main-title light">投稿辦法</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Submit"
+};
+</script>
+
+<style lang="scss" scoped>
+.submit-container {
+  padding: 45px 0;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -2,18 +2,21 @@
   <main>
     <Banner />
     <About />
+    <Submit />
   </main>
 </template>
 
 <script>
 import Banner from "@/components/homepage/Banner.vue";
 import About from "@/components/homepage/About.vue";
+import Submit from "@/components/homepage/Submit.vue";
 
 export default {
   name: "Homepage",
   components: {
     Banner,
-    About
+    About,
+    Submit
   }
 };
 </script>
