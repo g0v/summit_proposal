253f2a0a0b21b1888f8bf6ec5cc126713e44d3a3
UnaLai <qqq323618@gmail.com>
2020-05-26 11:30:30 +0800

    [feat]:add FAQ(less)

---
+++ src/components/homepage/FAQ.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="faq">
+    <div class="faq-container container">
+      <h2 class="main-title drark">FAQ</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "FAQ"
+};
+</script>
+
+<style lang="scss" scoped>
+.faq {
+  background-color: $sub-color;
+}
+.faq-container {
+  padding: 45px 0;
+}
+</style>
--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -3,6 +3,7 @@
     <Banner />
     <About />
     <Submit />
+    <FAQ />
   </main>
 </template>
 
@@ -10,13 +11,15 @@
 import Banner from "@/components/homepage/Banner.vue";
 import About from "@/components/homepage/About.vue";
 import Submit from "@/components/homepage/Submit.vue";
+import FAQ from "@/components/homepage/FAQ.vue";
 
 export default {
   name: "Homepage",
   components: {
     Banner,
     About,
-    Submit
+    Submit,
+    FAQ
   }
 };
 </script>
