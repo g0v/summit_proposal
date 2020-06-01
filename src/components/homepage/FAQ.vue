9d1625a9f5a9dd548a6511c01fc113bae89fb441
UnaLai <qqq323618@gmail.com>
2020-06-01 12:08:22 +0800

    [feat]:update FAQ content

--- src/components/homepage/FAQ.vue
+++ src/components/homepage/FAQ.vue
@@ -2,14 +2,15 @@
   <section class="faq" id="faq">
     <div class="faq-container container">
       <h2 class="main-title drark">FAQ</h2>
+      <h3>投稿相關</h3>
       <div class="item">
         <div class="q">
           <b-icon icon="question-square-fill" variant="danger"></b-icon
-          >提案結果何時公佈？入選與未入選都會收到通知嗎？
+          >結果何時公佈？入選與未入選都會收到通知嗎？
         </div>
         <p class="a">
-          <b-icon icon="chat-dots" variant="dark"></b-icon>提案審查將於 ??
-          完成，我們將以 email 通知入選及未入選的提案人/團隊。
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >議程規劃將於八月底完成，我們將以 email 通知入選或未入選稿件。
         </p>
       </div>
       <div class="item">
@@ -18,10 +19,115 @@
           >投稿後，是否還可以修改投稿內容？
         </div>
         <p class="a">
-          <b-icon icon="chat-dots" variant="dark"></b-icon
-          >可以。本次大會採開放協作式的投稿系統。提案者送出提案後，投稿內容將會呈現在大會網站上，供大眾提問及評論。提案者在
-          xx 之前，都可利用提案系統與評論者互動，並修改提案。提案系統將於 xx
+          <b-icon icon="chat-dots" variant="dark"></b-icon>可以。g0v Summit 2020
+          採開放協作式的投稿系統。投稿者投稿後，內容將會呈現在系統網站上，開放提問及討論。投稿者在
+          7/30 之前，都可以透過網站與公眾互動，並修改稿件內容。系統將於 8/2
           關閉修改功能。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >一個人可以參與多個稿件嗎？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >可以。議程委員會依講者多元性、稿件內容等條件綜合評量。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >我因有特殊考慮，不希望被錄影、錄音、拍照、直播，或任何形式的記錄，這樣適合投稿嗎？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >可以。投稿系統上將有此欄位供投稿者勾選。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >是否可以以團隊為單位投稿？如果我們是一個團隊的話，其他人會被列在網站上面嗎？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >可以。投稿時，請於「顯示於網頁的名字」留下團隊名字，「講者簡介」留下團隊簡介。「講者姓名」、「電子信箱」、「聯絡方式」則請留下團隊聯絡人資訊。若希望個別講者也列名於議程網站上，可於講者簡介中列舉講者。另一種方式是以多人為講者投稿（見前一題），但在「所屬單位或社群」欄位標註團隊名。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >可以多人報名一個短講嗎？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >可以。請在投稿系統上，分別列出參與講者的資訊。
+        </p>
+      </div>
+      <h3>議程需求</h3>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >我的議程將以非華語進行，大會是否能提供翻譯協助？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >部分場地與時段可協助安排口譯，請於提案時備註使用語言。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >我的議程將以非華語進行，大會是否能提供翻譯協助？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >部分場地與時段可協助安排口譯，請於提案時備註使用語言。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >我的議程有需要特殊協助（例如手語、無障礙空間等等），大會是否可以協助安排？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >請於投稿時備註需求，大會將在考量場地與成本限制之餘，將儘可能協助安排。
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >會議場地有提供什麼器材？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >各場地均備有基礎會議設備，包括投影、麥克風、直播等設備。若有特殊需求，請於提案時備註。
+        </p>
+      </div>
+      <h3>交通、住宿相關</h3>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >我不住在台南，大會是否會協助講者安排住宿與交通？或是提供補助？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >很抱歉，此次大會並不提供任何住宿與交通協助。歡迎大家開發台南好吃好住好玩的地點！
+        </p>
+      </div>
+      <div class="item">
+        <div class="q">
+          <b-icon icon="question-square-fill" variant="danger"></b-icon
+          >我是外國人，大會是否會協助我辦理簽證與入境相關文件？
+        </div>
+        <p class="a">
+          <b-icon icon="chat-dots" variant="dark"></b-icon
+          >我們將會在通知入選時一併寄送邀請函，供簽證及入境申請使用。其它簽證辦理相關問題，請參考外交部領事事務局網站並洽詢台灣駐各地辦事處。若有需要主辦單位提供任何文件，請與我們聯繫
+          <a href="mailto:programming@summit.g0v.tw。"
+            >programming@summit.g0v.tw。</a
+          >
         </p>
       </div>
     </div>
@@ -42,6 +148,12 @@ export default {
   padding: 45px 0;
   h2 {
     margin-bottom: 45px;
+  }
+  h3 {
+    margin: 25px 0;
+    text-align: left;
+    color: #fff;
+    font-size: 23px;
   }
   .item {
     text-align: left;
