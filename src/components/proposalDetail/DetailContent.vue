8680f92949b65bf3b1531d3c7833c2028baf1f03
UnaLai <qqq323618@gmail.com>
2020-06-04 00:43:42 +0800

    [feat]:add speaker

--- src/components/proposalDetail/DetailContent.vue
+++ src/components/proposalDetail/DetailContent.vue
@@ -1,37 +1,66 @@
 <template>
   <div class="detail-content">
     <div class="detail-content-container container">
-      <div class="side">
-        <div class="item">
-          <h4>修訂紀錄</h4>
-          <div class="content record">
-            <span
-              v-for="(version, index) in projectDetail.versions"
-              :key="index"
-              >version {{ index }}</span
-            >
+      <div class="main-info">
+        <div class="side">
+          <div class="item">
+            <h4>修訂紀錄</h4>
+            <div class="content record">
+              <span
+                v-for="(version, index) in projectDetail.versions"
+                :key="index"
+                >version {{ index }}</span
+              >
+            </div>
+          </div>
+        </div>
+        <main>
+          <div class="item">
+            <h3>摘要 Summary</h3>
+            <p>{{ latestVersion.summary }}</p>
+            <p>{{ latestVersion.summary_en }}</p>
+          </div>
+          <div class="item">
+            <h3>形式 Format</h3>
+            <p>{{ latestVersion.format }}</p>
+          </div>
+          <div class="item">
+            <h3>主題分類 Topic</h3>
+            <p>{{ latestVersion.topic }}</p>
+          </div>
+          <div class="item">
+            <h3>三個關鍵字 3 keywords</h3>
+            <p>{{ latestVersion.keywords.join(" , ") }}</p>
+          </div>
+        </main>
+      </div>
+      <div class="speaker">
+        <div
+          class="item"
+          v-for="(speaker, index) in latestVersion.speakers"
+          :key="index"
+        >
+          <div class="pic">
+            <img :src="speaker.avatar_url" alt="speaker" />
+          </div>
+          <div class="open-info">
+            <h4>講者名稱 Speaker’s display name</h4>
+            <p>{{ speaker.display_name }}</p>
+            <h4>
+              組織或社群名稱 Speaker’s organization or community affiliation
+            </h4>
+            <p>{{ speaker.organization }}</p>
+            <h4>講者所在城市 Speaker’s location (city)</h4>
+            <p>{{ speaker.city }}</p>
+            <h4>講者資訊連結 Speaker info URL</h4>
+            <p><a :href="speaker.info_url" target="_blank">前往外部連結</a></p>
+            <h4>講者簡介 Short bio</h4>
+            <p>{{ speaker.bio }}</p>
+            <h4>Speaker’s bio in English</h4>
+            <p>{{ speaker.bio_en }}</p>
           </div>
         </div>
       </div>
-      <main>
-        <div class="item">
-          <h3>摘要 Summary</h3>
-          <p>{{ latestVersion.summary }}</p>
-          <p>{{ latestVersion.summary_en }}</p>
-        </div>
-        <div class="item">
-          <h3>形式 Format</h3>
-          <p>{{ latestVersion.format }}</p>
-        </div>
-        <div class="item">
-          <h3>主題分類 Topic</h3>
-          <p>{{ latestVersion.topic }}</p>
-        </div>
-        <div class="item">
-          <h3>三個關鍵字 3 keywords</h3>
-          <p>{{ latestVersion.keywords.join(" , ") }}</p>
-        </div>
-      </main>
     </div>
   </div>
 </template>
@@ -57,65 +86,136 @@ export default {
 
 <style lang="scss" scoped>
 .detail-content-container {
-  padding: 20px;
-  background-color: #ece8e8;
-  display: flex;
-  justify-content: space-between;
-  .side {
-    width: 20%;
-  }
-  main {
-    width: 75%;
-  }
-  .side {
-    text-align: left;
-    .item {
-      margin-bottom: 30px;
-      h4 {
-        font-size: 16px;
-        color: #000;
-        border-bottom: 3px solid $main-color;
-        text-align: center;
-        padding: 5px 0;
-        font-weight: 600;
-      }
-      .content {
-        img {
-          width: 50px;
-          height: 50px;
-          object-fit: cover;
-          border-radius: 50%;
-          margin: 8px;
+  .main-info {
+    padding: 50px 20px;
+    box-shadow: 0 0 8px #ccc5c5;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+    border-radius: 5px;
+    .side {
+      width: 20%;
+    }
+    main {
+      width: 75%;
+    }
+    .side {
+      text-align: left;
+      .item {
+        margin-bottom: 30px;
+        h4 {
+          font-size: 16px;
+          color: #000;
+          border-bottom: 3px solid $main-color;
+          text-align: center;
+          padding: 5px 0;
+          font-weight: 600;
         }
-        span {
+        .content {
+          img {
+            width: 50px;
+            height: 50px;
+            object-fit: cover;
+            border-radius: 50%;
+            margin: 8px;
+          }
+          span {
+            display: block;
+          }
+        }
+      }
+    }
+    main {
+      text-align: left;
+      color: #000;
+      .item {
+        margin-bottom: 50px;
+        position: relative;
+        &:not(:last-child)::after {
+          content: "";
           display: block;
+          width: 100%;
+          height: 2px;
+          background-color: #827d7d1f;
+          position: absolute;
+          bottom: -25px;
+          left: 50%;
+          transform: translateX(-50%);
         }
+      }
+      h3 {
+        font-size: 20px;
+      }
+      p {
+        margin-bottom: 0;
       }
     }
   }
-  main {
-    text-align: left;
-    color: #000;
+  .speaker {
     .item {
-      margin-bottom: 50px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: left;
+      background-color: #292828;
+      color: #fff;
+      padding: 45px 0;
+      border-radius: 5px;
+      margin-bottom: 30px;
+      box-shadow: 0 0 8px #ccc5c5;
+    }
+    .pic {
+      text-align: center;
+      margin-bottom: 40px;
       position: relative;
-      &:not(:last-child)::after {
+      img {
+        width: 130px;
+        height: 130px;
+        object-fit: cover;
+        border-radius: 50%;
+        border: 2px solid #fff;
+        position: relative;
+        z-index: 1;
+        background-color: gray;
+      }
+      &::before {
         content: "";
-        display: block;
-        width: 100%;
-        height: 2px;
-        background-color: #827d7d1f;
+        width: 100px;
+        height: 100px;
+        background-color: $main-color;
+        border-radius: 50%;
         position: absolute;
-        bottom: -25px;
-        left: 50%;
-        transform: translateX(-50%);
+        right: -30%;
+        top: -15px;
+        z-index: 0;
+      }
+      &::after {
+        content: "";
+        width: 50px;
+        height: 50px;
+        background-color: $main-color;
+        border-radius: 50%;
+        position: absolute;
+        left: -20px;
+        bottom: -7px;
+        z-index: 1;
       }
     }
-    h3 {
-      font-size: 20px;
-    }
-    p {
-      margin-bottom: 0;
+    .open-info {
+      width: 90%;
+      h4 {
+        font-size: 18px;
+        color: #ef4645;
+      }
+      p {
+        font-size: 15px;
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+      a {
+        color: #fff;
+      }
     }
   }
 }
