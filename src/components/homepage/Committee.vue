dc4a663cfe6e63dc1d5d0cb738274fc53fd158c4
UnaLai <qqq323618@gmail.com>
2020-06-07 21:04:03 +0800

    [feat]:update Committee content

--- src/components/homepage/Committee.vue
+++ src/components/homepage/Committee.vue
@@ -14,10 +14,19 @@
             多項跨界專案，分別和不同專業的共同作者將相關經驗整理成
             Blupa、Blulu、ETCC 等圖解模型，於包括 g0v summit
             在內的研討會發表。已淡出社群多年，可望在議程討論中提供外部的觀點。
-            興趣是轉行，曾從事臨床醫療、科學繪圖、企畫管理、介面設計，最近剛轉行到程式設計。
+            興趣是轉行，曾從事臨床醫療、科學繪圖、企畫管理、介面設計，最近剛轉行到程式設計。<br />ET
+            had participated in various cross-disciplinary g0v projects and had
+            shared those experiences in several conferences including previous
+            g0v summits. Her perspective was presented as illustrated metrics /
+            models including Blupa, Blulu and ETCC, co-authored with experts
+            from different backgrounds. We can expect ET to provide some
+            outsider’s views since she has been faded out from the community for
+            a couple of years. ET is interested in making career changes and
+            used to work as a dentist, a medical illustrator, a planner /
+            manager, an UI designer, and the latest one: a programmer.
           </b-card-text>
           <template v-slot:header>
-            <small>網頁前端工程師</small>
+            <small>網頁前端工程師<br />Web Frontend Engineer</small>
           </template>
         </b-card>
 
@@ -30,10 +39,14 @@
           <b-card-text>
             g0v
             社群參與者，不務正業的法律人程序控，長年的台日交流小幫手，平常遊走於公私部門之間的協調者，2016
-            年意外跌進了 vTaiwan 的坑 ，和大家一起探索開放社群永續發展的極限。
+            年意外跌進了 vTaiwan 的坑
+            ，和大家一起探索開放社群永續發展的極限。<br />A g0v contributor who
+            builds communities and works between the public and private sectors.
+            Mainly contributed in vTaiwan project, exploring the possibility of
+            open communities.
           </b-card-text>
           <template v-slot:header>
-            <small>自由人</small>
+            <small>自由人<br />Yorozuya</small>
           </template>
         </b-card>
 
@@ -63,10 +76,19 @@
         >
           <b-card-text>
             希望能夠紀錄、觀察、理解網路科技對社會影響的文字工作者。關注網路X人權、網路X政治、開放政府和網路科技的商業發展。賺錢求生的工作包括商業周刊、金融時報記者，華陽創投董事長特助。實現理想的工作是在台南開一家咖啡店。
-            跟g0v的相遇是在報導完德國海盜黨之後，對Summit的期待是為更多人創造同等美好且有意義的相遇，而後開始一些事情。
+            跟g0v的相遇是在報導完德國海盜黨之後，對Summit的期待是為更多人創造同等美好且有意義的相遇，而後開始一些事情。<br />With
+            9 years of experience in journalism and Internet industry, Jason Liu
+            is a Taipei-based journalist focusing on human rights, digital
+            politics and digital economics. His works have won several major
+            journalism awards, and have been published in international media,
+            including Business Weekly Taiwan, Financial Times, Initium Media and
+            The Reporter. He recently investigates the digital surveillance and
+            disinformation warfare in the Greater China region.
           </b-card-text>
           <template v-slot:header>
-            <small>報導者主筆</small>
+            <small
+              >報導者主筆<br />Senior Writer, The Reporter Foundation</small
+            >
           </template>
         </b-card>
         <b-card
@@ -79,10 +101,13 @@
             任職於經典工程顧問有限公司(http://www.classic1990.com/)，持續以社群志願者的身份，參與「零時政府揪松團
             (g0v Jothon)」、「公有地大行動 (POP on Fire)」、「資料申請小幫手
             (Dataopener.tw)」、「都市農耕網 (Farming Urbanism
-            Network)」等開放知識公民社群的行動。
+            Network)」等開放知識公民社群的行動。<br />Chewei works at Classic
+            Landscape Design and Environmental Planning. Ltd
+            (http://www.classic1990.com/), and continuously participates in 'g0v
+            Jothon' 'POP On Fire', 'dataopener.tw', 'Farming Urbanism Network'.
           </b-card-text>
           <template v-slot:header>
-            <small>零時政府揪松團志工</small>
+            <small>零時政府揪松團志工<br />Volunteer of g0v Jothon</small>
           </template>
         </b-card>
 
@@ -94,10 +119,19 @@
         >
           <b-card-text>
             畢業於嘉義大學生物機電工程研究所，曾任職傳產系統工程師。喜愛學新技術及自造小玩意，碰巧搭上創客運動風潮，2016在Fablab
-            Academy結業。在南方創客基地服務，主要負責場地設備維護管理、課程規畫執行及專案開發。
+            Academy結業。在南方創客基地服務，主要負責場地設備維護管理、課程規畫執行及專案開發。<br />I
+            got an MA in Department of Biomechatronic Engineering of Chiayi
+            University. And be a systems engineer for a few years in food
+            industries. I love learning new technologies and self-made gadgets.
+            I happened to catch up with the Maker movement, and got the certify
+            of Fablab Academy in 2016. Since then I working for Fablab STMC,
+            including maintenance and management of equipment, course planning
+            and execution, and project development.
           </b-card-text>
           <template v-slot:header>
-            <small>南方創客基地 技術總監</small>
+            <small
+              >南方創客基地 技術總監<br />Fablab STMC Technical Director</small
+            >
           </template>
         </b-card>
         <b-card
@@ -107,7 +141,15 @@
           img-top
         >
           <b-card-text>
-            從中山大學社會系畢業後就到新芽工作，現在四年了。即使現在從中央到地方政府都強調資料開放，但在地方議會，無論是議會預決算、會議資料、會議記錄等，都是非常不足夠，卻也少有人關注的。期待能在summit中得到更多催促議會進步的靈感。
+            從中山大學社會系畢業後就到新芽工作，現在四年了。即使現在從中央到地方政府都強調資料開放，但在地方議會，無論是議會預決算、會議資料、會議記錄等，都是非常不足夠，卻也少有人關注的。期待能在summit中得到更多催促議會進步的靈感。<br />I
+            now work for Tainan Sprout, an organization mainly focuses on local
+            issues.Ｉn local council, we may find out that regardless of
+            governmental budget bills or agenda related documents are not only
+            lacking but short of attention.Hope to develop ways to make local
+            council better in summit. Watching local council, we may find out
+            that regardless of governmental budget bills or agenda related
+            documents are not only lacking but short of attention. Hope to
+            develop ways to make local council better in summit.
           </b-card-text>
           <template v-slot:header>
             <small>台南新芽秘書長</small>
@@ -124,10 +166,15 @@
             space，並且提供免費的資訊培育，以 mentor
             引導、學員自主學習的方式，量產好品質的工程師。好想工作室也是台南資訊社群聚集地，舉辦超過
             600
-            場公開講座，同時對接國內外的業界，讓從事資訊相關的年輕人可以根留台南。
+            場公開講座，同時對接國內外的業界，讓從事資訊相關的年輕人可以根留台南。<br />Launches
+            a IT working space, and focus the topic to software. Doing free
+            training program for people who want to be a developer. Dedicates to
+            building Tainan, the city of Southern Taiwan to become a IT light
+            tower and a manufacturer of producing quality developer in Taiwan.
+            Builds an online platform of “Mask Inventory Map” in 2020.
           </b-card-text>
           <template v-slot:header>
-            <small>好想工作室 創辦人</small>
+            <small>好想工作室 創辦人<br />Goodideas-Studio, the Host</small>
           </template>
         </b-card>
 
