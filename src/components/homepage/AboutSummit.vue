8e923d18b0365738f20fbdfb07219d89bc4f9af2
UnaLai <qqq323618@gmail.com>
2020-06-07 20:30:14 +0800

    [feat]:update about content

--- src/components/homepage/AboutSummit.vue
+++ src/components/homepage/AboutSummit.vue
@@ -9,9 +9,9 @@
         </h3>
         <p>
           g0v Summit
-          雙年會兩年舉辦一次，關注開放政府、開源協作、公民參與等面向，是國際開放政府社群的焦點活動，過去三屆吸引多達
+          國際雙年會兩年舉辦一次，關注開放政府、開源協作、公民參與等面向，是國際開放政府社群的焦點活動，過去三屆吸引多達
           33
-          國講者投稿。雙年會全部由社群參與者志願籌辦，議程也秉持開放協作精神，全部議程皆有口譯、文字記錄和直播。議程除了大會籌辦，也開放完整時段的開放工作坊（Unconference）由參與者提案決定議程內容，以演講、討論、自由對話等形式創造新的協作空間。閉幕前自由報名的全場閃電講更是開源社群傳統，以開放分享的精神貫穿年會。
+          國講者投稿。雙年會大部份由社群貢獻者志願籌辦，議程秉持開放協作精神，籌備大會的討論文件皆以共筆形式公開，也鼓勵參與者協作議程的文字紀錄，會中也有現場口譯和影像直播輔助，減少參與的負擔。議程除了大會籌辦，也開放完整時段的開放工作坊（Unconference）由參與者提案決定議程內容，以演講、討論、自由對話等形式創造新的協作空間。會中自由報名的全場閃電講更是開源社群傳統，以開放分享的精神貫穿年會。
         </p>
         <p>
           g0v Summit is a biennial gathering focuses on open government,
@@ -43,7 +43,7 @@
         </h4>
         <p>
           歷屆 g0v Summit
-          皆有來自全球各國的講者、參與者來到台灣，帶著豐富的經驗，透過演講、討論、工作坊，與近千名與會者交流經驗。<br />In
+          皆有來自全球各國的參與者來到台灣，帶著豐富的經驗，透過演講、討論、工作坊，與近千名會眾交流經驗。<br />In
           previous g0v Summits, participants with rich experience from all over
           the world came to Taiwan. They exchanged experiences with nearly 1,000
           congregations through speeches, discussions, and workshops.
@@ -68,7 +68,7 @@
           Communities
         </h4>
         <p>
-          g0v Summit 2020 首次離開台北，在台南舉辦。g0v Summit
+          g0v Summit 2020 首次離開台北，在台南舉辦。g0v Summit 2020
           工作小組與台南在地伙伴協作一年，連結台南及鄰近縣市的 maker
           社群、NGO、文化工作者、教育工作者，讓習慣虛擬空間的公民黑客與在實體空間中奔走的社群貢獻者們齊聚一堂，聆聽彼此的故事，用對話了解彼此。<br />g0v
           Summit 2020 leave Taipei for the first time and will be held in
@@ -88,13 +88,12 @@
         <p>
           相較於過去的 g0v Summit，為期 2-3 天，在單一會場集中舉行，g0v Summit
           2020 擴大參與，聯合在地社群，推出連續 4
-          天的黑客松、短講、主題論壇、自由對話與開放工作坊，活動場所也擴大到 5
-          個會場，包含各在地社群經營多年的空間與群眾。<br />Compared to the
-          previous g0v Summit, which lasted for 2-3 days with a single venue,
-          g0v Summit 2020 expanded its participation and connected the local
-          communities, launching 4 consecutive days of hackathons, short talks,
-          themed forums, free dialogue, and unconference. Workshops and event
-          venues have also been extended to several locations in different
+          天的黑客松、短講、主題論壇、自由對話與開放工作坊，活動場所也擴大到散佈在市區的數個會場，包含各在地社群經營多年的空間與群眾。<br />Compared
+          to the previous g0v Summit, which lasted for 2-3 days with a single
+          venue, g0v Summit 2020 expanded its participation and connected the
+          local communities, launching 4 consecutive days of hackathons, short
+          talks, themed forums, free dialogue, and unconference. Workshops and
+          event venues have also been extended to several locations in different
           places in the urban area, including spaces and masses that have been
           operated by local communities for many years.
         </p>
